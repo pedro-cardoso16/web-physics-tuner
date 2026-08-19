767781d08baa8b6db58c7ab9c1daabbd01392282
Pedro Cardoso <pedro.cardoso.dev@gmail.com>
2026-08-19 09:00:14 -0300

    Move logic to model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,35 @@
 import torch
+import torch.nn as nn
+from typing import Any
+
+
+class MLP(nn.Module):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+        nn.ParameterDict(
+            {
+                "k": nn.Parameter(torch.tensor([], dtype=torch.float32)),
+            }
+        )
+
+        self.layers = nn.Sequential(
+            nn.Linear(7, 100),
+            nn.ReLU(),
+            nn.Linear(100, 100),
+            nn.ReLU(),
+            nn.Linear(100, 100),
+            nn.ReLU(),
+            nn.Linear(100, 2),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.layers(x)
+        return x
+
+if __name__ == "__main__":
+    loss = nn.functional.mse_loss
+    x = torch.tensor([1],dtype=torch.float32)
+    gt = torch.tensor([5],dtype=torch.float32)
+    print(loss(x,gt))
+    
