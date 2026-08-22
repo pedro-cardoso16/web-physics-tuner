e8fa306e130eab6cb8e52603cc27c2c0e01b6424
Pedro Cardoso <pedro.cardoso.dev@gmail.com>
2026-08-22 19:00:07 -0300

    Apply some normalization to data

--- src/dataset_generator.py
+++ src/dataset_generator.py
@@ -1,5 +1,7 @@
 import numpy as np
 import sys
+import pandas as pd
+from tools import rotate_particles
 
 # from copy import deepcopy
 import json
@@ -44,9 +46,16 @@ def extract_nodes_properties(simulation: Simulation):
             variables.pop("particle", None)
             variables.pop("x1", None)
             variables.pop("x2", None)
-            variables.pop("outer_particle_1", None)
-            variables.pop("outer_particle_2", None)
-            variables.pop("central_particle", None)
+            outer_particle_1 = variables.pop("outer_particle_1", None)
+            outer_particle_2 = variables.pop("outer_particle_2", None)
+            central_particle = variables.pop("central_particle", None)
+
+            if particles[i] == central_particle:
+                force_type += "_central"
+            elif particles[i] == outer_particle_1:
+                force_type += "_outer_1"
+            elif particles[i] == outer_particle_2:
+                force_type += "_outer_2"
 
             for key, val in variables.items():
                 if isinstance(val, np.ndarray):
@@ -97,34 +106,76 @@ def execution(seed=None, **kwargs):
     simulation.build_vectorized_constraints()
 
     data = []
-    for _ in range(kwargs.get("n", 100)):
+    for _ in range(kwargs.get("n", kwargs.get("n_iterations", 50))):
         simulation.run(n=1)
+        properties = extract_nodes_properties(simulation)
+        data.extend(properties)
 
-        data.extend(extract_nodes_properties(simulation))
+    x_min = data[0]["input"]["x"][0]
+    x_max = x_min
+    y_min = data[0]["input"]["x"][1]
+    y_max = y_min
+    # normalization
+
+    for p in data:
+        # find min max values of the x and y coords
+        x_min = min(p["input"]["x"][0], x_min, p["target"][0])
+        x_max = max(p["input"]["x"][0], x_max, p["target"][0])
+        y_min = min(p["input"]["x"][1], y_min, p["target"][1])
+        y_max = max(p["input"]["x"][1], y_max, p["target"][1])
+
+    for p in data:
+        p["input"]["x"][0] -= x_min
+        p["input"]["x"][1] -= y_min
+        p["target"][0] -= x_min
+        p["target"][1] -= y_min
+
+        x_range = x_max - x_min
+        y_range = y_max - y_min
+
+        for i, r in enumerate((x_range, y_range)):
+            if r == 0:
+                p["input"]["x"][i] = 0
+                p["target"][i] = 0
+                p["input"]["v"][i] = 0
+            else:
+                p["input"]["x"][i] /= r
+                p["target"][i] /= r
+                p["input"]["v"][i] /= r
+
+        p["input"]["n"] = (p["input"]["n"] - 1) / 100
 
     return data
 
 
 def generate_dataset(
-    file_path: str | None = None, seed: int | None = None, n_simulations: int = 50
+    file_path: str | None = None,
+    seed: int | None = None,
+    n_simulations: int = 50,
+    normalize: bool = True,
+    **kwargs,
 ):
-    # if seed is None:
-    #     seed = int(random.randint(0,sys.maxsize))
-
     data = []
     futures = []
 
     rng = np.random.default_rng(seed)
 
-    with ProcessPoolExecutor() as executor:
+    with ProcessPoolExecutor(kwargs.get("max_workers", None)) as executor:
         for _ in range(n_simulations):
             simulation_seed = rng.integers(0, sys.maxsize)
 
-            future = executor.submit(execution, simulation_seed)
+            future = executor.submit(
+                execution,
+                **{
+                    "seed": simulation_seed,
+                    "n_iterations": kwargs.get("n_iterations", 50),
+                },
+            )
 
             futures.append(future)
 
         position = 0
+
         for future in tqdm(
             as_completed(futures),
             desc="Generating synthetic dataset",
@@ -135,20 +186,21 @@ def generate_dataset(
             position += 1
 
     if file_path is not None:
-        write_large_jsonl(data, file_path)
+        write_large_json(data, file_path)
 
     return data
 
 
-
-def write_large_jsonl(data, filepath, chunk_size=50000):
+def write_large_json(data, filepath, chunk_size=50000):
     with open(filepath, "w", encoding="utf-8") as f:
         f.write("[\n")
-        for i in range(0, len(data), chunk_size):
+        for i in tqdm(
+            range(0, len(data), chunk_size), desc="Writing to json file", unit="chunk"
+        ):
             chunk = data[i : i + chunk_size]
             f.write(",\n".join(map(json.dumps, chunk)) + "\n")
         f.write("]\n")
 
 
 if __name__ == "__main__":
-    generate_dataset("data/train_data.json", 15, 100)
+    generate_dataset("data/train_data.json", 15, 5)
